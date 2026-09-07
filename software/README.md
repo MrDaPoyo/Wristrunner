f4106e1d44317b4442f7826cb2b9e1f14bd859a2
poyo <webmaster@poy.ooo>
2026-09-07 12:55:48 +0200

    beginning to understand Gio

--- software/README.md
+++ software/README.md
@@ -1,1 +1,9 @@
-# this folder will contain the software 
+# Wristrunner Launcher
+Uses [Gio](https://gioui.org/doc/install/linux)
+
+## Installation
+Gio requires 
+```sh
+sudo pacman -S --needed vulkan-headers vulkan-tools
+go install gioui.org/cmd/gogio@latest
+```
---
+++ software/sdk/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"os"
+
+	"gioui.org/app"
+	"gioui.org/op"
+	"gioui.org/widget/material"
+)
+
+func main() {
+	// the event loop
+	go func() {
+		w := new(app.Window)
+		th := material.NewTheme() // theme customizes colors, shapes, fonts, etc.
+		var ops op.Ops            // records a buffer that tells Gio what to draw and handle and applies them all at once.
+
+		for {
+			switch e := w.Event().(type) {
+			case app.DestroyEvent:
+				os.Exit(0) // clean exit
+			case app.FrameEvent:
+				gtx := app.NewContext(&ops, e)                           // the event holds data about the window such as animation timers and size.
+				material.Label(th, 16, "Hello Wristrunners").Layout(gtx) // adds a title.
+				e.Frame(gtx.Ops)                                         // draw to screen.
+			}
+		}
+	}()
+
+	app.Main() // keeps the event loop alive
+}
