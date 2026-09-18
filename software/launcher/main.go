805b68e50fd800e5fa353b42f21ea16537154ad1
poyo <webmaster@poy.ooo>
2026-09-18 21:26:00 +0200

    slight formatting

--- software/launcher/main.go
+++ software/launcher/main.go
@@ -103,74 +103,76 @@ func appGrid(gtx C, th *material.Theme) D {
 	app7, app8, app9 := widget.Clickable{}, widget.Clickable{}, widget.Clickable{}
 	margins := layout.UniformInset(unit.Dp(5))
 	return margins.Layout(gtx, func(gtx C) D {
-		{return layout.Flex{Axis: layout.Vertical}.Layout(gtx,
-			layout.Flexed(1, func(gtx C) D {
-				return layout.Flex{Axis: layout.Horizontal}.Layout(gtx,
-					layout.Flexed(1, func(gtx C) D {
-						return margins.Layout(gtx, func(gtx C) D {
-							return material.Button(th, &app1, "App 1").Layout(gtx)
-						})
-					}),
-					layout.Flexed(1, func(gtx C) D {
-						return margins.Layout(gtx, func(gtx C) D {
-							return material.Button(th, &app2, "App 2").Layout(gtx)
-						})
-					}),
-					layout.Flexed(1, func(gtx C) D {
-						return margins.Layout(gtx, func(gtx C) D {
-							return material.Button(th, &app3, "App 3").Layout(gtx)
-						})
-					}),
-				)
-			}),
-			layout.Flexed(1, func(gtx C) D {
-				return layout.Flex{Axis: layout.Horizontal}.Layout(gtx,
-					layout.Flexed(1, func(gtx C) D {
-						return margins.Layout(gtx, func(gtx C) D {
-							return material.Button(th, &app4, "App 4").Layout(gtx)
-						})
-					}),
-					layout.Flexed(1, func(gtx C) D {
-						return margins.Layout(gtx, func(gtx C) D {
-							return material.Button(th, &app5, "App 5").Layout(gtx)
-						})
-					}),
-					layout.Flexed(1, func(gtx C) D {
-						return margins.Layout(gtx, func(gtx C) D {
-							return material.Button(th, &app6, "App 6").Layout(gtx)
-						})
-					}),
-				)
-			}),
-			layout.Flexed(1, func(gtx C) D {
-				return layout.Flex{Axis: layout.Horizontal}.Layout(gtx,
-					layout.Flexed(1, func(gtx C) D {
-						return margins.Layout(gtx, func(gtx C) D {
-							return material.Button(th, &app7, "App 7").Layout(gtx)
-						})
-					}),
-					layout.Flexed(1, func(gtx C) D {
-						return margins.Layout(gtx, func(gtx C) D {
-							return material.Button(th, &app8, "App 8").Layout(gtx)
-						})
-					}),
-					layout.Flexed(1, func(gtx C) D {
-						return margins.Layout(gtx, func(gtx C) D {
-							return material.Button(th, &app9, "App 9").Layout(gtx)
-						})
-					}),
-				)
-			}),
-		)}
+		{
+			return layout.Flex{Axis: layout.Vertical}.Layout(gtx,
+				layout.Flexed(1, func(gtx C) D {
+					return layout.Flex{Axis: layout.Horizontal}.Layout(gtx,
+						layout.Flexed(1, func(gtx C) D {
+							return margins.Layout(gtx, func(gtx C) D {
+								return material.Button(th, &app1, "App 1").Layout(gtx)
+							})
+						}),
+						layout.Flexed(1, func(gtx C) D {
+							return margins.Layout(gtx, func(gtx C) D {
+								return material.Button(th, &app2, "App 2").Layout(gtx)
+							})
+						}),
+						layout.Flexed(1, func(gtx C) D {
+							return margins.Layout(gtx, func(gtx C) D {
+								return material.Button(th, &app3, "App 3").Layout(gtx)
+							})
+						}),
+					)
+				}),
+				layout.Flexed(1, func(gtx C) D {
+					return layout.Flex{Axis: layout.Horizontal}.Layout(gtx,
+						layout.Flexed(1, func(gtx C) D {
+							return margins.Layout(gtx, func(gtx C) D {
+								return material.Button(th, &app4, "App 4").Layout(gtx)
+							})
+						}),
+						layout.Flexed(1, func(gtx C) D {
+							return margins.Layout(gtx, func(gtx C) D {
+								return material.Button(th, &app5, "App 5").Layout(gtx)
+							})
+						}),
+						layout.Flexed(1, func(gtx C) D {
+							return margins.Layout(gtx, func(gtx C) D {
+								return material.Button(th, &app6, "App 6").Layout(gtx)
+							})
+						}),
+					)
+				}),
+				layout.Flexed(1, func(gtx C) D {
+					return layout.Flex{Axis: layout.Horizontal}.Layout(gtx,
+						layout.Flexed(1, func(gtx C) D {
+							return margins.Layout(gtx, func(gtx C) D {
+								return material.Button(th, &app7, "App 7").Layout(gtx)
+							})
+						}),
+						layout.Flexed(1, func(gtx C) D {
+							return margins.Layout(gtx, func(gtx C) D {
+								return material.Button(th, &app8, "App 8").Layout(gtx)
+							})
+						}),
+						layout.Flexed(1, func(gtx C) D {
+							return margins.Layout(gtx, func(gtx C) D {
+								return material.Button(th, &app9, "App 9").Layout(gtx)
+							})
+						}),
+					)
+				}),
+			)
+		}
 	})
 }
 
 func alertWidgets(gtx C, th *material.Theme) D {
-	
+
 	margins := layout.Inset{
-		Top:   unit.Dp(5),
-		Left:  unit.Dp(10),
-		Right: unit.Dp(10),
+		Top:    unit.Dp(5),
+		Left:   unit.Dp(10),
+		Right:  unit.Dp(10),
 		Bottom: unit.Dp(12),
 	}
 	return layout.Flex{Axis: layout.Vertical, Alignment: layout.Middle}.Layout(gtx,
@@ -188,7 +190,7 @@ func alertWidgets(gtx C, th *material.Theme) D {
 			return margins.Layout(gtx, func(gtx C) D {
 				return material.Label(th, 16, "  I").Layout(gtx)
 			})
-		}),	
+		}),
 		layout.Rigid(func(gtx C) D {
 			return margins.Layout(gtx, func(gtx C) D {
 				return material.Label(th, 16, " S").Layout(gtx)
