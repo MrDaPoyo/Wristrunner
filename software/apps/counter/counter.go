a453c17aa17d50d55335f795b380e4a5fdf0f933
poyo <webmaster@poy.ooo>
2026-09-24 08:09:26 +0200

    rephrase +1 button in counter, add decrement button to counter

--- software/apps/counter/counter.go
+++ software/apps/counter/counter.go
@@ -12,6 +12,7 @@ import (
 type App struct {
 	count int
 	inc   widget.Clickable
+	dec   widget.Clickable
 }
 
 func New() uikit.App { return &App{} }
@@ -20,14 +21,20 @@ func (a *App) Layout(gtx layout.Context, th *uikit.Theme) layout.Dimensions {
 	if a.inc.Clicked(gtx) {
 		a.count++
 	}
+	if a.dec.Clicked(gtx) {
+		a.count--
+	}
 
 	return layout.Center.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
-		return layout.Flex{Axis: layout.Vertical, Alignment: layout.Middle}.Layout(gtx,
+		return layout.Flex{Axis: layout.Vertical, Alignment: layout.Middle, Spacing: layout.SpaceBetween, Gap: th.Spacing}.Layout(gtx,
 			layout.Rigid(func(gtx layout.Context) layout.Dimensions {
-				return material.Body1(th.Theme, "count = "+strconv.Itoa(a.count)).Layout(gtx)
+				return material.Body1(th.Theme, strconv.Itoa(a.count)).Layout(gtx)
 			}),
 			layout.Rigid(func(gtx layout.Context) layout.Dimensions {
-				return material.Button(th.Theme, &a.inc, "increment").Layout(gtx)
+				return material.Button(th.Theme, &a.inc, "+1").Layout(gtx)
+			}),
+			layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+				return material.Button(th.Theme, &a.dec, "-1").Layout(gtx)
 			}))
 	})
 }
