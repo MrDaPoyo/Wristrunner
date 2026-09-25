67a4132547f36b07064305af42095f7c9ca50406
poyo <webmaster@poy.ooo>
2026-09-25 08:45:45 +0200

    better spacing

--- software/apps/notes/list.go
+++ software/apps/notes/list.go
@@ -27,9 +27,14 @@ func (a *App) layoutList(gtx layout.Context, th *uikit.Theme) layout.Dimensions 
 		return a.layoutEdit(gtx, th)
 	}
 
-	inset := layout.UniformInset(unit.Dp(th.Spacing))
+	inset := layout.Inset{
+		Top:    unit.Dp(th.Spacing) / 2,
+		Bottom: unit.Dp(th.Spacing) / 2,
+		Left:   unit.Dp(th.Spacing),
+		Right:  unit.Dp(th.Spacing),
+	}
 	return layout.Flex{Axis: layout.Vertical}.Layout(gtx,
-		layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+		layout.Flexed(0.1, func(gtx layout.Context) layout.Dimensions {
 			return material.Button(th.Theme, &a.addBtn, "New note").Layout(gtx)
 		}),
 		layout.Flexed(1, func(gtx layout.Context) layout.Dimensions {
